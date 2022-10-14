165e5e4673bd801ca315284a883cd241f5b33983
yeyae <whyjinny@naver.com>
2022-10-14 22:30:17 +0900

    python

---
+++ 52.py
@@ -0,0 +1,12 @@
+#모듈만들기
+#mod1.py
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a-b
+
+    #모듈 불러오기
+    #mod1.py를 불러오기 위해서는 다음과 같이 명령 프롬프트 창을 열고
+    #mod1.py를 저장한 디렉터리로 이동한 다음 대화형 인터프리터를 실행
+    
